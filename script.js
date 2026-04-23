57d4b84697e3dab13f158b7c3d3d09fe419e99af
Zer1ch <v14.nazar@gmail.com>
2026-04-23 22:26:08 +0300

    оновити сайт

--- script.js
+++ script.js
@@ -8,3 +8,29 @@
             menu.classList.toggle('active');
             burger.classList.toggle('active'); // ДОДАЙ ЦЕЙ РЯДОК: Тепер бургер теж знає, що він натиснутий
         });
+        // Знаходимо всі кнопки "Замовити консультацію"
+const consultBtns = document.querySelectorAll('.open-consult-btn');
+const consultOverlay = document.getElementById('consultOverlay');
+const consultModal = document.getElementById('consultModal');
+const closeConsultBtn = document.getElementById('closeConsultBtn');
+
+// Функція відкриття
+function openConsult() {
+    consultOverlay.style.display = 'block';
+    consultModal.style.display = 'block';
+}
+
+// Функція закриття
+function closeConsult() {
+    consultOverlay.style.display = 'none';
+    consultModal.style.display = 'none';
+}
+
+// Вішаємо подію кліку на КОЖНУ кнопку на сторінці
+consultBtns.forEach(function(btn) {
+    btn.addEventListener('click', openConsult);
+});
+
+// Закриття по хрестику та кліку на розмитий фон
+closeConsultBtn.addEventListener('click', closeConsult);
+consultOverlay.addEventListener('click', closeConsult);
